f9712ccc89464cbcd4957fdc5c76b33a92156868
Sebastien Delpeuch <sebastien.delpeuch@enseirb-matmeca.fr>
2022-03-08 19:52:53 +0100

    ✨ feat: add record function and downlable file

--- configure_cameleon.py
+++ configure_cameleon.py
@@ -1,5 +1,7 @@
+import os
+
 from cv2 import cv2
-from flask import Blueprint, render_template, request
+from flask import Blueprint, render_template, request, send_file
 
 
 class ConfigureCameleon:
@@ -9,9 +11,12 @@ class ConfigureCameleon:
         self.recording = False
         self.share = False
         self.camera_list = []
+        self.file = []
 
         self.bp.route('/scan', methods=['GET'])(self.scan)
         self.bp.route('/choose_device', methods=['POST'])(self.choose_device)
+        self.bp.route('/files', methods=['GET'])(self.list_video)
+        self.bp.route('/files/<filename>', methods=['GET'])(self.get_file)
 
     def scan(self):
         # Find availables cameras
@@ -48,5 +53,25 @@ class ConfigureCameleon:
                 self.share = True
         except KeyError:
             self.share = False
-        return render_template('index.html', devices=self.camera_list, device_id=self.camera, recording=self.recording,
-                               share=self.share)
+        return render_template('index.html', devices=self.camera_list, device_id=self.camera)
+
+    def list_video(self):
+        print(self.camera_list, self.camera)
+        self.file = os.listdir('record/')
+        if len(self.file) == 0:
+            return render_template(template_name_or_list='index.html', files=self.file)
+        self.file.sort(reverse=True)
+        return render_template(template_name_or_list='index.html', files=self.file)
+
+    def get_file(self, filename):
+        file_path = 'record/' + filename
+        if not os.path.exists(file_path) or filename == '':
+            return render_template('404.html')
+
+            # Check if path is a file and serve
+        if os.path.isfile(file_path):
+            return send_file(file_path, as_attachment=True)
+
+            # Show directory contents
+        files = sorted(os.listdir(file_path))
+        return render_template('index.html', files=files, devices=self.camera_list, device_id=self.camera)
